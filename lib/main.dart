253edb20c5aa99a2a172a4d71743c1c692233e15
Laxman Bhattarai <erluxman@gmail.com>
2021-10-03 15:56:55 +0545

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart';
 import 'package:geolocation_sample/green_log.dart';
 import 'package:geolocation_sample/sensor_plugin.dart';
-import 'package:sensortracker/sensortracker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home_screen.dart';
@@ -12,40 +11,6 @@ void main() async {
   BackgroundGeolocation.registerHeadlessTask(headlessTask);
   await GreenLogs.logInfo("App started", "this is just a simple log");
   SensorPlugin.startSensorService();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  final strings = await getHeadlessEventsFromSharedPrefs();
-  await addStringToPrefs("Namaste //split payload");
-  await prefs.setDouble("testabc", 200);
-  await dummyPlugin();
-}
-
-Future<void> dummyPlugin() async {
-  await GreenLogs.logInfo(
-      "Before Sensortracker.platformVersion", "this is just a simple log");
-  String version = "NoversionAvailiable";
-  try {
-    version = await Sensortracker.platformVersion;
-  } catch (e, trace) {
-    await GreenLogs.logError(
-        "Failed to get platformVersion ", e.toString(), trace.toString());
-    Logger.error("Failed to get platformVersion  ${e.toString()}");
-  }
-  await GreenLogs.logInfo(
-      "After Sensortracker.platformVersion", "version is $version");
-
-  await GreenLogs.logInfo("Before SensortrackerCopy.lastHeadlessUpdate",
-      "this is just a simple log");
-  double lastStored = 0;
-  try {
-    lastStored = DateTime.now().millisecondsSinceEpoch.toDouble();
-    await Sensortracker.lastHeadlessUpdate(lastStored);
-  } catch (e, trace) {
-    await GreenLogs.logError(
-        "Failed to get platformVersion ", e.toString(), trace.toString());
-    Logger.error("Failed to get platformVersion  ${e.toString()}");
-  }
-  await GreenLogs.logInfo(
-      "After SensortrackerCopy.lastHeadlessUpdate", "version is $lastStored");
 }
 
 Future<void> headlessTask(HeadlessEvent headlessEvent) async {
@@ -55,36 +20,12 @@ Future<void> headlessTask(HeadlessEvent headlessEvent) async {
   final String newEntry = "$nameOfEvent//split$payload";
   GreenPrefs greenPrefs = await GreenPrefs.getInstance();
   final prefs = await SharedPreferences.getInstance();
-  prefs.setDouble(
-      "lastHeartbeat", DateTime.now().millisecondsSinceEpoch.toDouble());
+  final dateNow = DateTime.now();
+  prefs.setDouble("lastHeartbeat", dateNow.millisecondsSinceEpoch.toDouble());
   await greenPrefs.putHeadlessEvent(newEntry);
 
-  await GreenLogs.logInfo(
-      "Before Sensortracker.platformVersion", "this is just a simple log");
-  String version = "NoversionAvailiable";
-  try {
-    version = await Sensortracker.platformVersion;
-  } catch (e, trace) {
-    await GreenLogs.logError(
-        "Failed to get platformVersion ", e.toString(), trace.toString());
-    Logger.error("Failed to get platformVersion  ${e.toString()}");
-  }
-  await GreenLogs.logInfo(
-      "After Sensortracker.platformVersion", "version is $version");
-
-  await GreenLogs.logInfo("Before SensortrackerCopy.lastHeadlessUpdate",
-      "this is just a simple log");
-  double lastStored = 0;
-  try {
-    lastStored = DateTime.now().millisecondsSinceEpoch.toDouble();
-    await Sensortracker.lastHeadlessUpdate(lastStored);
-  } catch (e, trace) {
-    await GreenLogs.logError(
-        "Failed to get platformVersion ", e.toString(), trace.toString());
-    Logger.error("Failed to get platformVersion  ${e.toString()}");
-  }
-  await GreenLogs.logInfo(
-      "After SensortrackerCopy.lastHeadlessUpdate", "version is $lastStored");
+  await GreenLogs.logInfo("New lastHeartbeat add",
+      "New log added as value ${dateNow.toIso8601String()}");
 }
 
 Future<List<String>> getHeadLessEventsListFromHive() async {
--- sensortracker/example/android/app/src/main/kotlin/com/greenplay/sensortracker/sensortracker_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.greenplay.sensortracker.sensortracker_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
